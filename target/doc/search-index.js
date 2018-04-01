f6cc4b15b949addb46f7950f3adf3c8d667c7610
Wim Looman <wim@nemo157.com>
2018-04-01 11:38:58 +0200

    Regenerate docs with rustdoc 1.26.0-nightly (ae544ee1c 2018-03-29)

--- target/doc/search-index.js
+++ target/doc/search-index.js
@@ -1,4 +1,6 @@
 var searchIndex = {};
 searchIndex["proc_macro_re_export"] = {"doc":"","items":[],"paths":[]};
 searchIndex["proc_macro_re_export_inner"] = {"doc":"","items":[[5,"the_macro","proc_macro_re_export_inner","",null,{"inputs":[{"name":"tokenstream"},{"name":"tokenstream"}],"output":{"name":"tokenstream"}}]],"paths":[]};
+searchIndex["proc_macro_re_export_normal_macro"] = {"doc":"","items":[[14,"the_normal_macro","proc_macro_re_export_normal_macro","",null,null]],"paths":[]};
+searchIndex["proc_macro_re_export_proc_macro"] = {"doc":"","items":[[5,"the_proc_macro","proc_macro_re_export_proc_macro","",null,{"inputs":[{"name":"tokenstream"},{"name":"tokenstream"}],"output":{"name":"tokenstream"}}]],"paths":[]};
 initSearch(searchIndex);
