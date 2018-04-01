dab2d834ce9df22c2895a3915e462c9ef1c5b977
Wim Looman <wim@nemo157.com>
2018-04-01 11:24:53 +0200

    Add docs generated with rustdoc 1.26.0-nightly (ae544ee1c 2018-03-29)

---
+++ target/doc/search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = {};
+searchIndex["proc_macro_re_export"] = {"doc":"","items":[],"paths":[]};
+searchIndex["proc_macro_re_export_inner"] = {"doc":"","items":[[5,"the_macro","proc_macro_re_export_inner","",null,{"inputs":[{"name":"tokenstream"},{"name":"tokenstream"}],"output":{"name":"tokenstream"}}]],"paths":[]};
+initSearch(searchIndex);
